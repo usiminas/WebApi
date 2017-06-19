3169418724e78171174d3d6efae001d43db12ab6
Peterson Golfredo <peterson.golfredo@outlook.com>
2017-06-19 19:38:46 -0300

    teste 1

--- WebApi/WebApi.Web/Controllers/WebApiController .cs
+++ WebApi/WebApi.Web/Controllers/WebApiController .cs
@@ -10,16 +10,19 @@ namespace WebApi.Web.Controllers
 {
     public class WebApiController : ApiController
     {
-
+        [HttpGet]
         public IHttpActionResult Get()
         {
             return Content(HttpStatusCode.OK, new Retorno() { Error = 0,  MsgError = "List", Object = new string[] { "value1", "value2" } }, Configuration.Formatters.JsonFormatter);
         }
 
+        [HttpGet]
         public IHttpActionResult Save()
         {
             return Content(HttpStatusCode.OK, new Retorno() { Error = 0, MsgError = "Save", Object = new string[] { "value1", "value2" } }, Configuration.Formatters.JsonFormatter);
         }
+
+        [HttpGet]
         public IHttpActionResult Remove()
         {
             return Content(HttpStatusCode.OK, new Retorno() { Error = 0, MsgError = "Remove", Object = new string[] { "value1", "value2" } }, Configuration.Formatters.JsonFormatter);
